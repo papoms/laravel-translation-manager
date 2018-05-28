e71d50513f3815ad2de4b47726cd56dc7d4ebe0d
Paul Porzky <paulporzky@gmail.com>
2018-05-28 22:33:50 +0200

    routes

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -86,7 +86,13 @@ class ManagerServiceProvider extends ServiceProvider {
         $router->group($config, function($router)
         {
             $router->get('view/{group}', 'Controller@getView');
-            $router->get('/', 'Controller');
+            $router->get('/{group?}', 'Controller@getIndex');
+            $router->post('/add', 'Controller@postAdd');
+            $router->post('/edit/{group}', 'Controller@postEdit');
+            $router->post('/delete', 'Controller@postDelete');
+            $router->post('/import', 'Controller@postImport');
+            $router->post('/find', 'Controller@postFind');
+            $router->post('/publish', 'Controller@postPublish')
         });
 	}
 
